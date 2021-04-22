97e43eaf8aa306491bc13e5b155d54d350047008
mrueda020 <mrueda020@hotmail.com>
2021-04-22 18:50:33 -0500

    Added Job component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import useFetchJobs from "./useFetchJobs";
-
+import Job from "./Job";
 function App() {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
   return (
-    <Container>
+    <Container className="my-4">
+      <h1 className="mb-4">Github Jobs</h1>
       {loading && <h1>loading</h1>}
       {error && <h1>error</h1>}
-      {jobs && <h1>{console.log(jobs)}</h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job}></Job>;
+      })}
     </Container>
   );
 }
